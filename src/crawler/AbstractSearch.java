3aa73824d39bba036837cdbc79474e4d1aeb70a8
Endalkachew <eb808826@gmail.com>
2021-02-16 14:42:32 +0300

    :rocket: Full implemented the search functionality of the Search abstract class and main class

--- src/crawler/AbstractSearch.java
+++ src/crawler/AbstractSearch.java
@@ -3,16 +3,18 @@ package crawler;
 import java.io.File;
 import java.io.FilenameFilter;
 
+/**
+ * Instances of classes that implement this interface are used to 
+ * filter filenames. These instances are used to filter directory 
+ * listings in the list method of class File, and by the Abstract 
+ * Window Toolkit's file dialog component.
+ */
 public abstract class AbstractSearch implements FilenameFilter {
     String initials;
 
-    public AbstractSearch(String initials) {
-        this.initials = initials;
-    }
-
-    public AbstractSearch() {
-
-    }
-
+    /**
+     * accept method from the FilenameFilter class
+     * Tests if a specified file should be included in a file list.
+     */
     public abstract boolean accept(File dir, String name);
 }
--- src/crawler/Search.java
+++ src/crawler/Search.java
@@ -4,6 +4,14 @@ import java.io.File;
 
 public class Search extends AbstractSearch {
 
+    public Search(String initials) {
+        this.initials = initials;
+	}
+
+	/**
+     * accept method from the FilenameFilter class
+     * Tests if a specified file should be included in a file list.
+     */
     @Override
     public boolean accept(File dir, String name) {
         return name.startsWith(initials);
