f0d322d0bdd4f234f2e7ef0853a919d53a7603d4
Endalkachew <eb808826@gmail.com>
2021-02-27 07:56:49 +0300

    :bulb: :rocket: finals Scan class

--- src/crawler/Scan.java
+++ src/crawler/Scan.java
@@ -6,26 +6,23 @@
 
 package crawler;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Set;
 
 public class Scan {
 
     private File root_directory; // The root directory to scan
     private String file_name; // The target filename
-    private int number_of_result = 0;
     private int recursive_index = 3; // recursive index. directory tree depth
 
     /**
      * Main API class to interface with backend logic.
+     * 
      * @param root_directory : The root directory to scan
      * @param file_name
      */
@@ -36,10 +33,11 @@ public class Scan {
 
     /**
      * Main API class to interface with backend logic.
-     * @param root_directory : The root directory to scan
+     * 
+     * @param root_directory  : The root directory to scan
      * @param file_name
-     * @param recursive_index : The maxDepth parameter is the maximum number of levels
-     * of directories to visit.
+     * @param recursive_index : The maxDepth parameter is the maximum number of
+     *                        levels of directories to visit.
      */
     public Scan(File root_directory, String file_name, int recursive_index) {
         this.set_file_name(file_name);
@@ -47,7 +45,7 @@ public class Scan {
         this.set_recursive_index(recursive_index);
     }
 
-    public void set_recursive_index(int recursive_index) { 
+    public void set_recursive_index(int recursive_index) {
         this.recursive_index = recursive_index;
     }
 
@@ -55,23 +53,11 @@ public class Scan {
         return this.recursive_index;
     }
 
-    public int get_number_of_result() {
-        return this.number_of_result;
-    }
-
-    /**
-     * increment_result
-     * increment the number of files found matching the given specification.
-     */
-    public void increment_result() {
-        this.number_of_result++;
-    }
-
     public void set_root_directory(File root_directory) {
         this.root_directory = root_directory;
     }
 
-    public  File get_root_directory() {
+    public File get_root_directory() {
         return this.root_directory;
     }
 
@@ -84,8 +70,9 @@ public class Scan {
     }
 
     /**
-     * scan_filter_by_ext:
-     * method for filtering the file scan result using given file extension
+     * scan_filter_by_ext: method for filtering the file scan result using given
+     * file extension
+     * 
      * @param file_extension
      * @return list of files that matches the given filter parameter
      * @throws IOException
@@ -98,26 +85,76 @@ public class Scan {
         return found_files;
     }
 
-    public List<File> scan() throws IOException, UncheckedIOException {
+    public List<File> scan() throws IOException, UncheckedIOException, AccessDeniedException, NoSuchFileException {
+
         List<File> found_files = new ArrayList<File>();
-
-        // create Files instance with the root directory
-        Stream<Path> traverse_tree = Files.walk(
-                Paths.get(this.get_root_directory().toString()), this.get_recursive_index());
-
-        List<File> files = traverse_tree.filter(Files::isRegularFile).map(x -> x.toFile())
-                                                                    .collect(Collectors.toList());
-
-        traverse_tree.close();
-
-        for (File file : files) {
-            if (file.toString().toLowerCase().contains(this.get_file_name().toLowerCase())) {
-                this.increment_result();
-                found_files.add(file);
-            }
-        }
+        
+        Set<FileVisitOption> fileVisitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(
+            Paths.get(this.get_root_directory().toURI()), 
+            fileVisitOptions, this.get_recursive_index(), new FileVisitorImplementation(this.get_file_name(), found_files)
+        );
 
         return found_files;
+    }
+
+    public static void main(String[] args) throws IOException{
+        List<File> files = new ArrayList<>();
+        Set<FileVisitOption> fileVisitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get("C:\\Users\\endalk\\source\\"), fileVisitOptions, 2, new FileVisitorImplementation("README", files));
+        System.out.println(files.size());
+    }
+
+}
+
+class FileVisitorImplementation implements FileVisitor<Path> {
+
+    private List<File> files;
+    private String file_name;
+
+    public FileVisitorImplementation(String file_name, List<File> files) {
+        this.file_name = file_name;
+        this.files = files;
+    }
+
+    /**
+     * This method is invoked before a directory’s entries are visited i.e., 
+     * this method is called for each directory in the tree before its children 
+     * (contents/entries of the directory) are visited.
+     */
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    /**
+     * postVisitDirectory: Called after all the directory entries are visited.
+     * If there were any exceptions encountered during the traversal, the 
+     * exception will be passed as an argument to this method. 
+     */
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    /**
+     * visitFile: Invoked for each file visited in the tree.
+     */
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.toString().toLowerCase().contains(this.file_name.toLowerCase())) {
+            this.files.add(file.toFile());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    /**
+     * visitFileFailed: Called when the file cannot be accessed. The exception
+     * encountered is passed to the method.
+     */
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
     }
 
 }
