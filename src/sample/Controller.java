d5509ece1302722ecd2536e5cd850f0832070964
Endalkachew <eb808826@gmail.com>
2021-02-28 21:35:14 +0300

    :rocket: Fronend UI controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,74 @@
 package sample;
 
+import crawler.Scan;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.io.File;
+import java.util.List;
 
 public class Controller {
 
-    public Label demo_label;
+    public Button scan_btn;
+    public TextField filename_field;
+    public CheckBox shallow_scan_field;
+    public CheckBox deep_scan_field;
+    public Label message_board;
+    public ListView<String> result_list;
 
-    public void sayHello(ActionEvent actionEvent) {
-        demo_label.setText("Hello World!");
+    public void scan(ActionEvent actionEvent) {
+        String filename = filename_field.getText();
+        boolean deep_scan = deep_scan_field.isSelected();
+        boolean shallow_scan = shallow_scan_field.isSelected();
+
+        if (deep_scan) {
+            filename_field.setText("scanning for " + filename + " in deep scan mode");
+            try {
+                Scan scanner = new Scan(filename, Integer.MAX_VALUE);
+                List<File> found_files = scanner.scan();
+                int found_files_size = found_files.size();
+
+                ObservableList<String> listItems = FXCollections.observableArrayList();
+
+                for(File file : found_files) {
+                    listItems.add(file.toString());
+                }
+                result_list.setItems(listItems);
+
+                message_board.setText("Found " + found_files_size + " files");
+            } catch (Exception error_message) {
+
+            }
+        } else {
+            message_board.setText("scanning for " + filename + " in shallow scan mode");
+            try {
+                Scan scanner = new Scan(filename);
+                List<File> found_files = scanner.scan();
+                int found_files_size = found_files.size();
+
+                ObservableList<String> listItems = FXCollections.observableArrayList();
+
+                for(File file : found_files) {
+                    listItems.add(file.toString());
+                }
+                result_list.setItems(listItems);
+
+                message_board.setText("Found " + found_files_size + " files");
+            } catch (Exception error_message) {
+
+            }
+        }
+    }
+
+    public void change_deep_scan(ActionEvent actionEvent) {
+        message_board.setText("Deep scan may consume a lot of CPU and Disk resource");
+        shallow_scan_field.setSelected(false);
+    }
+
+    public void change_shallow_scan(ActionEvent actionEvent) {
+        message_board.setText("Enter filename and scan.");
+        deep_scan_field.setSelected(false);
     }
 }
