82dbc34867c88bf911b8a88f1b070b8628d4febd
Endalkachew <eb808826@gmail.com>
2021-02-28 19:53:57 +0300

    :sprinkles: :bulb: Final version of the backend for scanning the disk for the specified file

--- src/crawler/Scan.java
+++ src/crawler/Scan.java
@@ -1,14 +1,10 @@
-/**
- * Scan
- * Main backend class that scans the drive for the specified file. Scans the disk
- * recursively with filename and file extension.
- */
 
 package crawler;
 
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
@@ -16,89 +12,125 @@ import java.util.Set;
 
 public class Scan {
 
-    private File root_directory; // The root directory to scan
-    private String file_name; // The target filename
-    private int recursive_index = 3; // recursive index. directory tree depth
+    private List<File> ROOT_DIRECTORIES;
 
-    /**
-     * Main API class to interface with backend logic.
-     * 
-     * @param root_directory : The root directory to scan
-     * @param file_name
-     */
-    public Scan(File root_directory, String file_name) {
-        this.set_root_directory(root_directory);
-        this.set_file_name(file_name);
-    }
+    private int recursive_index;
 
-    /**
-     * Main API class to interface with backend logic.
-     * 
-     * @param root_directory  : The root directory to scan
-     * @param file_name
-     * @param recursive_index : The maxDepth parameter is the maximum number of
-     *                        levels of directories to visit.
-     */
-    public Scan(File root_directory, String file_name, int recursive_index) {
-        this.set_file_name(file_name);
-        this.set_root_directory(root_directory);
-        this.set_recursive_index(recursive_index);
-    }
+    private String file_name;
+    private String file_extension;
+    private LocalDate file_created;
 
-    public void set_recursive_index(int recursive_index) {
-        this.recursive_index = recursive_index;
-    }
-
-    public int get_recursive_index() {
-        return this.recursive_index;
-    }
-
-    public void set_root_directory(File root_directory) {
-        this.root_directory = root_directory;
-    }
-
-    public File get_root_directory() {
-        return this.root_directory;
-    }
-
-    public void set_file_name(String file_name) {
+    public Scan(String file_name) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = 4;
         this.file_name = file_name;
     }
 
-    public String get_file_name() {
-        return this.file_name;
+    public Scan(String file_name, int recursive_index) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = recursive_index;
+        this.file_name = file_name;
     }
 
-    /**
-     * scan_filter_by_ext: method for filtering the file scan result using given
-     * file extension
-     * 
-     * @param file_extension
-     * @return list of files that matches the given filter parameter
-     * @throws IOException
-     */
-    public List<File> filter_by_ext(String file_extension) throws IOException {
+    public Scan(String file_name, String file_extension) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = 4;
+        this.file_name = file_name;
+        this.file_extension = file_extension;
+    }
+
+    public Scan(String file_name, String file_extension, int recursive_index) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = recursive_index;
+        this.file_name = file_name;
+        this.file_extension = file_extension;
+    }
+
+    public Scan(String file_name, LocalDate file_created) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = 4;
+        this.file_name = file_name;
+        this.file_created = file_created;
+    }
+
+    public Scan(String file_name, LocalDate file_created, int recursive_index) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = recursive_index;
+        this.file_name = file_name;
+        this.file_created = file_created;
+    }
+
+    public Scan(String file_name, String file_extension, LocalDate file_created) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = 4;
+        this.file_name = file_name;
+        this.file_extension = file_extension;
+        this.file_created = file_created;
+    }
+
+    public Scan(String file_name, String file_extension, LocalDate file_created, int recursive_index) {
+        this.ROOT_DIRECTORIES = Utils.get_root_directories();
+        this.recursive_index = 4;
+        this.file_name = file_name;
+        this.file_extension = file_extension;
+        this.file_created = file_created;
+    }
+
+    public List<File> scan() throws Exception {
+        Utils.write_logfile(this.file_name);
         List<File> found_files = new ArrayList<File>();
 
-        // finish this functionality
+        if (this.recursive_index == 4) {
+            // Shalow scan with three recursive index
+            // Check if there is file extension filter
+            // Check if there is file created filter
 
+            for (File root_directory : this.ROOT_DIRECTORIES) {
+                Set<FileVisitOption> fileVisitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+                Files.walkFileTree(
+                    Paths.get(root_directory.toURI()), 
+                    fileVisitOptions, this.recursive_index, new FileVisitorImplementation(
+                        this.file_name, found_files)
+                );
+            }
+        } else {
+            for (File root_directory : this.ROOT_DIRECTORIES) {
+                Set<FileVisitOption> fileVisitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+                Files.walkFileTree(
+                    Paths.get(root_directory.toURI()), 
+                    fileVisitOptions, this.recursive_index, new FileVisitorImplementation(
+                        this.file_name, found_files)
+                );
+            }
+        }
         return found_files;
     }
 
-    public List<File> scan() throws IOException, UncheckedIOException, AccessDeniedException, NoSuchFileException {
+    private void filter(String file_extension) {
 
-        List<File> found_files = new ArrayList<File>();
-        
-        Set<FileVisitOption> fileVisitOptions = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
-        Files.walkFileTree(
-            Paths.get(this.get_root_directory().toURI()), 
-            fileVisitOptions, this.get_recursive_index(), new FileVisitorImplementation(this.get_file_name(), found_files)
-        );
-
-        return found_files;
     }
 
+    private void filter(LocalDate file_created) {
+
+    }
+
+    private void filter(String file_extension, LocalDate file_created) {
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scan scanner = new Scan("READ");
+            System.out.println("Found " + scanner.scan().size());
+            // for (File file : scanner.scan()) {
+            //     System.out.println(file);
+            // }
+        } catch(Exception error_message) {
+            System.out.println(error_message);
+        }
+    }
 }
+
 
 class FileVisitorImplementation implements FileVisitor<Path> {
 
@@ -137,6 +169,7 @@ class FileVisitorImplementation implements FileVisitor<Path> {
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (file.toString().toLowerCase().contains(this.file_name.toLowerCase())) {
             this.files.add(file.toFile());
+            System.out.println(file);
         }
         return FileVisitResult.CONTINUE;
     }
--- src/crawler/utils.java
+++ src/crawler/utils.java
@@ -12,146 +12,76 @@ import java.time.format.DateTimeFormatter;
 
 public class Utils {
 
-    public static String[] default_windows_dir = {"Desktop", "Documents", "Videos", "Downloads", "Pictures"};
-
+    public static String log_file_directory = "C:\\Users\\" + 
+                System.getProperty("user.name") + "\\file_crawler_log_file.csv";
     /**
-     * parse_username parses the username of the user. The username is used to
-     * prepare common directories for the given user such as
-     * "C:/Users/username/Desktop"
-     * @return string: username
+     * get_root_directories Returns list of root_directories to start traversing
+     * through.
      */
-    public static String parse_username() {
-        return System.getProperty("user.name");
-    }
+    public static List<File> get_root_directories() {
 
-    /**
-     * parse_drives parses the drives or partitions on the disk to crawle. This will
-     * be used to prepare the directories to crawle.
-     * 
-     * @return string[]: array of drives
-     */
-    public static List<File> parse_drives() {
-        File[] roots_list = File.listRoots();
+        List<File> ROOT_DIRECTORIES = new ArrayList<>();
 
-        List<File> drives = new ArrayList<>();
-
-        for (File drive : roots_list) {
-            drives.add(drive);
+        String[] default_c_directories_str = { "Desktop", "Documents", "Videos", "Downloads", "Pictures" };
+        for (String dir : default_c_directories_str) {
+            File directory = new File("C:\\Users\\" + System.getProperty("user.name") + "\\" + dir);
+            ROOT_DIRECTORIES.add(directory);
         }
 
-        return drives;
-    }
-
-    /**
-     * default_windows_dir
-     * returns collection of default windows directories in File instance
-     */
-    public static List<File> default_windows_dir() {
-        String username = Utils.parse_username();
-
-        List<File> default_dirs = new ArrayList<File>();
-
-        for (String def_dir : default_windows_dir) {
-            String dir = "C:\\Users\\" + username + "\\" + def_dir + "\\";
-            default_dirs.add(new File(dir));
+        for (File drive : File.listRoots()) {
+            File C_DRIVE = new File("C:\\");
+            if (drive.equals(C_DRIVE)) {
+                continue;
+            }
+            ROOT_DIRECTORIES.add(drive);
         }
-        return default_dirs;
+
+        return ROOT_DIRECTORIES;
     }
 
-    /**
-     * is_directory return boolean value by evaluating wether the given File
-     * instance is a directory or not. needed for validation purposes.
-     * 
-     * @param dir: the directory in question
-     * @return boolean value wether it is directory or not
-     */
-    public static boolean is_directory(File dir) {
-        return dir.isDirectory();
+    public static void write_logfile(String message) throws Exception {
+        FileWriter fwriter = new FileWriter(log_file_directory, true);
+        System.out.println(log_file_directory);
+        PrintWriter log_file = new PrintWriter(fwriter);
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formated_now = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formated_date = now.format(formated_now);
+
+        log_file.println(formated_date + "$" + message);
+
+        log_file.close();
     }
 
-    /**
-     * is_directory return boolean value by evaluating wether the given File
-     * instance represented by String input is a directory or not. needed for
-     * validation purposes.
-     * 
-     * @param dir: the directory in question
-     * @return boolean value wether it is directory or not
-     */
-    public static boolean is_directory(String dir) {
-        File file = new File(dir);
-        return file.isDirectory();
-    }
-
-    /**
-     * is_file return boolean value by evaluating wether the given File instance is
-     * a file or not. needed for validation purposes.
-     * 
-     * @param file: the directory in question
-     * @return boolean value wether it is directory or not
-     */
-    public static boolean is_file(File file) {
-        return file.isFile();
-    }
-
-    /**
-     * is_file return boolean value by evaluating wether the given File instance
-     * represented by String input is a file or not. needed for validation purposes.
-     * 
-     * @param dir: the directory in question
-     * @return boolean value wether it is directory or not
-     */
-    public static boolean is_file(String __file) {
-        File file = new File(__file);
-        return file.isDirectory();
-    }
-
-
-    public static void write_logfile(String message) {
-        try(
-            FileWriter fwriter = new FileWriter("log.log", true);
-            PrintWriter log_file = new PrintWriter(fwriter);
-        ) {
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter formated_now = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-            String formatted_date = now.format(formated_now);
-
-            log_file.println(formatted_date + "$" + message);
-        } catch(Exception error_message) {
-
-        }
-    }
-
-    public static List<String[]> read_logfile() {
+    public static List<String[]> read_logfile() throws Exception {
         List<String[]> processed_log = new ArrayList<>();
-        try {
-            File log_file = new File("log.log");
-            Scanner input_file = new Scanner(log_file);
 
-            List<String> log = new ArrayList<>();
+        File log_file = new File(log_file_directory);
+        Scanner input_file = new Scanner(log_file);
 
-            while (input_file.hasNext()) {
-                log.add(input_file.nextLine());
-            }
-            input_file.close();
+        List<String> log = new ArrayList<>();
 
-            for(String line : log) {
-                String message = line.substring((line.indexOf("$") + 1), line.length());
-                String timestamp = line.substring(0, (line.indexOf("$") - 1));
-
-                String[] log_line = { timestamp, message };
-                processed_log.add(log_line);
-                // System.out.println(processed_log.get(0)[0]);
-            }
-        } catch(Exception error_message) {
-
-        } finally {
-            return processed_log;
+        while (input_file.hasNext()) {
+            log.add(input_file.nextLine());
         }
+
+        input_file.close();
+
+        for (String line : log) {
+            String timestamp = line.substring(0, (line.indexOf("$") - 1));
+            String message = line.substring((line.indexOf("$") + 1), line.length());
+
+            String[] log_line = { timestamp, message };
+            processed_log.add(log_line);
+            // System.out.println(processed_log.get(0)[0]);
+        }
+
+        return processed_log;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         write_logfile("hello");
-        System.out.println(read_logfile().get(0)[1]);
+        System.out.println(read_logfile().get(0)[0] + " <-> " + read_logfile().get(0)[1] );
     }
 
 }
